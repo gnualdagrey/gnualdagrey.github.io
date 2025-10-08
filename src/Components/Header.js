549abb19c9d5ca0419596cec1493b3ae22d9ee11
Geoffrey Grey <gnualdagrey@gmail.com>
2025-10-08 14:37:31 +0800

    message

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h1>John Doe</h1>
+      <h1>Geoffrey Grey</h1>
       <p>Web Developer | Designer | Creator</p>
     </header>
   );
