586724e219e155df05563cf374a64cd9e53ef34a
Geoffrey Grey <gnualdagrey@gmail.com>
2025-10-08 14:53:25 +0800

    message

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,7 +10,7 @@ function About() {
         <div className="about-text">
           <p>Hello! I'm a passionate web developer with a love for creating beautiful and functional websites. I enjoy turning complex problems into simple, elegant solutions.</p>
           <p>I'm currently learning HTML, CSS, and JavaScript to build amazing web experiences. My goal is to become a full-stack developer and create applications that make a difference.</p>
-          <p>When I'm not coding, you can find me exploring new technologies, reading tech blogs, or working on personal projects. test</p>
+          <p>When I'm not coding, you can find me exploring new technologies, reading tech blogs, or working on personal projects.</p>
         </div>
       </div>
     </section>
