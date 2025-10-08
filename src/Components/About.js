a5b4ffc5caa12c858689600f0098c5e7e6267f39
Geoffrey Grey <gnualdagrey@gmail.com>
2025-10-08 14:30:33 +0800

    message

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,7 +4,9 @@ function About() {
     <section id="about">
       <h2>About Me</h2>
       <div className="about-content">
-        <div className="profile-image">👨‍💻</div>
+        <div className="profile-image">
+          <img src="public/cv.png"/>
+        </div>
         <div className="about-text">
           <p>Hello! I'm a passionate web developer with a love for creating beautiful and functional websites. I enjoy turning complex problems into simple, elegant solutions.</p>
           <p>I'm currently learning HTML, CSS, and JavaScript to build amazing web experiences. My goal is to become a full-stack developer and create applications that make a difference.</p>
